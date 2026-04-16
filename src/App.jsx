ec0557efacfe77f859db2c0b6c9a11b3c54148b5
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-16 17:54:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <h2>Email Password Authentication</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../pages/Layout/Root";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
